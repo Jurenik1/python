94799bfb8fbf3633913f34aa30f01acabdec5aaf
Angelica <angelica@sistema.com>
2018-02-14 16:28:56 +0100

    first commit

---
+++ edad.py
@@ -0,0 +1,6 @@
+﻿# -*- coding: utf-8 -*-
+#Calcular edad
+
+edad = input ("Pon el año que naciste: ")
+edad = 2018 - edad
+print "Tu edad es: "+str(edad)
---
+++ ejer.py
@@ -0,0 +1,7 @@
+
+
+num=100
+if num<=100:
+	print "Menor o igual"
+else:
+	print "Mayor que 100"
