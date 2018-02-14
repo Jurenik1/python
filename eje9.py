f20ea0ced311af1e412c998ac76d2fe40c55f64b
Angelica <angelica@sistema.com>
2018-02-14 19:41:01 +0100

    ejer

--- eje9.py
+++ eje9.py
@@ -1,3 +1,4 @@
 import mis_funciones
 
-print mis_funciones.par_impar(13)
+#print mis_funciones.par_impar(13)
+print mis_funciones.par_impar1(12)
--- mis_funciones.py
+++ mis_funciones.py
@@ -8,10 +8,20 @@ def funcion():
 	return "Hola mundo"
 
 	
-def par_impar(num):
-	if num%2==0:
-		return "par"
-	else:
-		return "impar"
+#def par_impar(num):
+#	if num%2==0:
+#		return "par"
+#	else:
+#		return "impar"
 		
 
+def par_impar1(num):
+	return "par" if (num%2==0) else "impar"
+	
+
+def datos (dni, nombre, *apellidos):
+	print dni
+	print nombre
+	print "Tienes"+str(len(apellidos))+" apellidos"
+	for argumento in apellidos:
+		print argumento
